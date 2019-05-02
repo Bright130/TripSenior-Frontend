2fb3bc4ec7a36dd35c87fb0865946d70adab4ca7
Bright <Bright130@hotmail.com>
2019-05-02 14:48:20 +0700

    insert button my trip / profile / changed logo img

--- src/mainpages/utility/header.js
+++ src/mainpages/utility/header.js
@@ -6,8 +6,6 @@ import Signin from "./signin";
 import { Button, Modal } from "semantic-ui-react";
 import PropTypes from "prop-types";
 
-
-
 function getBar(reactComponent) {
   if (localStorage.getItem("token") != null)
     return (
@@ -16,7 +14,12 @@ function getBar(reactComponent) {
         <div className="header-1-3-1">
           <div className="header-1-3-1-0">
             <div className="header-1-3-1-0-0">
-              <div className="header-my_trips_-6" onClick={reactComponent.goMyTrip.bind(reactComponent)}>My Trips</div>
+              <div
+                className="header-my_trips_-6"
+                onClick={reactComponent.goMyTrip.bind(reactComponent)}
+              >
+                My Trips
+              </div>
             </div>
           </div>
           <div className="header-line-4" />
@@ -31,16 +34,19 @@ function getBar(reactComponent) {
             </div>
           </div>
           <div className="header-1-4" />
-            <div className="header-1-5">
-              <div className="header-1-5-0" />
-              <div className="header-1-5-1" onClick={reactComponent.goProfile.bind(reactComponent)}>
-                <img
-                  src="https://ucarecdn.com/45490e93-6b20-48a6-a0fd-e118f76760dc/"
-                  className="header-image_5"
-                />
-              </div>
-              <div className="header-1-5-2" />
+          <div className="header-1-5">
+            <div className="header-1-5-0" />
+            <div
+              className="header-1-5-1"
+              onClick={reactComponent.goProfile.bind(reactComponent)}
+            >
+              <img
+                src="https://ucarecdn.com/45490e93-6b20-48a6-a0fd-e118f76760dc/"
+                className="header-image_5"
+              />
             </div>
+            <div className="header-1-5-2" />
+          </div>
         </div>
         <div className="header-1-3-2" />
       </div>
@@ -124,14 +130,14 @@ export default class Header extends React.Component {
                 <img
                   className="header-tripnotize_-2"
                   onClick={this.changeRoute}
-                  src="https://imgur.com/JXHy6wX.png"
+                  src="https://scontent.fbkk1-5.fna.fbcdn.net/v/t1.0-0/q85/p75x225/58444284_2296402160381997_4864408229200789504_o.jpg?_nc_cat=110&_nc_eui2=AeEmLxrBYx5gIScP8p724ufmjKlGGO4Z4GaM0Urlj_Sm6Yd6ffQvMkrfm1j0i4WQDraFJxPg62NKMInG-P8bKXyzuziBmEoz1hthXgvxiYSyHQ&_nc_ht=scontent.fbkk1-5.fna&oh=1cfac89a0c6591b0c64874b302d773ca&oe=5D6751BE"
                 />
               </div>
               <div className="header-1-1-2" />
             </div>
             <div className="header-1-2" />
             {getBar(this)}
-            
+
             <div className="header-1-6" />
           </div>
           <div className="header-2" />
